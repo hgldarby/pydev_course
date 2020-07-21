52542fa89743f84b221cacc21af303a266fbb2d8
Harry Darby <harry.darby@dematic.com>
2020-07-21 14:10:41 +0100

    added new files from section 3

---
+++ numbers.py
@@ -0,0 +1,14 @@
+# Fundamental Data Types
+# int and float
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+print(2 ** 3)
+print(2 // 4)
+print(5 % 4)
+
+# math functions
+print(round(3.1))
+print(abs(-4))
---
+++ python_data_types.py
@@ -0,0 +1,16 @@
+# Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes -> Custom Types
+
+# Specialised Data Types
+Modules
+
+None
